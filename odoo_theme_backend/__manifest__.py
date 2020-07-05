28a551305dba0db027c5bb360028e9f7478025d7
Haresh Kansara <hareshkansara>
2020-07-05 15:30:45 +0530

    [ADD] odoo_theme_backend: Added new module

---
+++ odoo_theme_backend/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Theme Backend',
+    'summary': 'Odoo Theme Backend',
+    'sequence': 15,
+    'description': "Odoo Theme Backend",
+    'depends': ['web'],
+    'data': [
+        'views/assets.xml',
+    ],
+}
